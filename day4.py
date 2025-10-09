964d1a35ca4ec01aae50b97b27316d55c6adb220
VENKATESH <154440420+venkatesh01-t@users.noreply.github.com>
2025-10-09 16:49:18 +0530

    Day 4

---
+++ day4.py
@@ -0,0 +1,19 @@
+#task 1
+for i in range(1,10,1):
+    print(i)
+#task 2
+a =int(input("Enterv the table value : "))
+for i in range(1,11):
+        print(f"{i} * {a} = {i*a}")
+
+#task 3
+li=[]
+for i in range(1,11):
+        #print(f"{i} * {a} = {i*a}")
+        li.append(i*a)
+print(li)
+
+"""while li:
+    print(li)
+    li.pop()"""
+    
